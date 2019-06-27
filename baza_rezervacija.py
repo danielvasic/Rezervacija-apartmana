d603de55518e1733dfa39e5f171e1ccfaf09e1f3
Daniel Vasic <dvasic1@gmail.com>
2019-06-27 15:53:10 +0200

    Self u SQL upitima nema smisla
    Self u SQL upitima nema smisla

--- baza_rezervacija.py
+++ baza_rezervacija.py
@@ -4,6 +4,6 @@ kon = connect("baza_ap.db")
 
 c = kon.cursor()
 
-c.execute("""CREATE TABLE tabla (ime_i_prezime TEXT, self.mjesto TEXT, self.naziv_apartmana TEXT, datum_boravka TEXT, broj_osoba INTEGER)""")
+c.execute("""CREATE TABLE tabla (ime_i_prezime TEXT, mjesto TEXT, naziv_apartmana TEXT, datum_boravka TEXT, broj_osoba INTEGER)""")
 
 kon.close()
